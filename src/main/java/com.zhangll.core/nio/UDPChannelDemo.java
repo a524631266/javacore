cc8d6763fe249c4c174a04f311aaba0c59f2b4a6
a524631266 <524631266@qq.com>
2020-07-15 23:35:37 +0800

    udp服务端基本实现

---
+++ src/main/java/com.zhangll.core/nio/UDPChannelDemo.java
@@ -0,0 +1,48 @@
+package com.zhangll.core.nio;
+
+import scala.Char;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.Channel;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.Selector;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
+/**
+ *nc -vu 127.０．０．０　８８８８
+ */
+public class UDPChannelDemo {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        DatagramChannel udp = DatagramChannel.open();
+        udp.bind(new InetSocketAddress(8888));
+
+        ByteBuffer allocate = ByteBuffer.allocate(100);
+
+//        Selector selector = Selector.open();
+        while (true){
+//            receive.wait();
+            allocate.clear();
+            udp.receive(allocate);
+//            int read = udp.read(allocate);
+//            allocate.rewind();
+//            udp.write(allocate);
+            allocate.flip();
+            Charset charset = Charset.forName("UTF-8");
+
+            CharsetDecoder decoder = charset.newDecoder();
+
+            CharBuffer charBuffer = decoder.decode(allocate);
+
+//            CharBuffer  charBuffer = decoder.decode(allocate.asReadOnlyBuffer());
+            // Byte转
+            System.out.println(charBuffer);
+        }
+
+    }
+}
