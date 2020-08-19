f394a68c0bee6ca7d431eb67c316499e1f56de3c
Zhang Liangliang <zhangll@homewell.com.cn>
2020-08-19 14:02:14 +0800

    添加简单的Server代码

---
+++ nettylearn/src/main/java/coderead/nio/netty/rpc/RpcServer.java
@@ -0,0 +1,36 @@
+package coderead.nio.netty.rpc;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class RpcServer {
+    public void openServer(int port) throws InterruptedException {
+        NioEventLoopGroup boss = new NioEventLoopGroup(1);
+        NioEventLoopGroup workers = new NioEventLoopGroup(8);
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        ChannelFuture bind = bootstrap.group(boss, workers)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel ch) throws Exception {
+                        ch.pipeline().addLast("codec", new RpcCodec());
+                        ch.pipeline().addLast("serverHandler", new ServerHandler());
+                    }
+                }).bind(port);
+        bind.sync().channel().close().sync();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        RpcServer rpcServer = new RpcServer();
+        rpcServer.openServer(8080);
+    }
+
+    private class ServerHandler extends SimpleChannelInboundHandler<Transfer> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, Transfer msg) throws Exception {
+            System.out.println("server receive client" + msg);
+        }
+    }
+}
