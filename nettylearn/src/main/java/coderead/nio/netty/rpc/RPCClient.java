db0d02e0abd8f0050ee5057b1594b415126b2f74
Zhang Liangliang <zhangll@homewell.com.cn>
2020-08-19 13:56:25 +0800

    添加客户端用来简单的向服务端发起请求的代码段

--- nettylearn/src/main/java/coderead/nio/netty/rpc/RPCClient.java
+++ nettylearn/src/main/java/coderead/nio/netty/rpc/RPCClient.java
@@ -1,15 +1,20 @@
 package coderead.nio.netty.rpc;
 
+import coderead.nio.netty.rpc.service.UserService;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
+import java.lang.reflect.Method;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicLong;
+
 /**
  * 发送消息
  */
 public class RPCClient {
-
+    public static AtomicLong idGenerator = new AtomicLong(0);
     private Channel channel = null;
 
     public void connect(String host, int port) throws InterruptedException {
@@ -26,10 +31,29 @@ public class RPCClient {
         channel = connect.sync().channel();
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, NoSuchMethodException {
         RPCClient rpcClient = new RPCClient();
         rpcClient.connect("localhost", 8080);
-        rpcClient.channel.pipeline();
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            Transfer msg = new Transfer(idGenerator.getAndIncrement(), false, true);
+            Class requestClass = UserService.class;
+            /**
+             * public interface UserService {
+             *     String getName(String name);
+             *     int getAge(int age);
+             * }
+             */
+//            Method requestMethod = requestClass.getMethod("getName");
+            String line = scanner.nextLine();
+            String[] parameters =  new String[]{line};
+            // 目前以string[]格式获取数，参数对象应该也是可序列化的对象
+            Request request = new Request(requestClass.getName(),"getName",parameters);
+            msg.updateTarget(request);
+            rpcClient.channel.writeAndFlush(msg);
+
+            scanner.close();
+        }
     }
 
     /**
@@ -39,7 +63,7 @@ public class RPCClient {
 
         @Override
         protected void channelRead0(ChannelHandlerContext ctx, Transfer msg) throws Exception {
-
+            System.out.println("server msg" + msg);
         }
     }
 }
