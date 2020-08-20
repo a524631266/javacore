bbbb553de6e621be7bbfb5aec5e26eedc1a59873
Zhang Liangliang <zhangll@homewell.com.cn>
2020-08-20 17:24:47 +0800

    修改编解码 + 添加protobuf配置文件

---
+++ nettylearn/src/main/java/coderead/nio/netty/rpc/serializer/protobuf/PtotobufSerializer.java
@@ -0,0 +1,17 @@
+package coderead.nio.netty.rpc.serializer.protobuf;
+
+import coderead.nio.netty.rpc.serializer.Serializer;
+
+import java.io.Serializable;
+
+public class PtotobufSerializer extends Serializer {
+    @Override
+    public byte[] serialize(Serializable target) {
+        return new byte[0];
+    }
+
+    @Override
+    public Object deSerialize(byte[] body) {
+        return null;
+    }
+}
