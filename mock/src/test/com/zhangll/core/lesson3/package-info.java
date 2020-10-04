69d5a6e4aeda05ccbdf6365af06815bb15e13b07
a524631266 <524631266@qq.com>
2020-10-04 16:04:42 +0800

    mock的第二种,annotation

--- mock/src/test/com/zhangll/core/lesson3/package-info.java
+++ mock/src/test/com/zhangll/core/lesson3/package-info.java
@@ -1,7 +1,15 @@
 /**
  * how to mock by Mockito
  * 1. use runner
+ *  @RunWith(MockitoJUnitRunner.class)
+ * 2. use annotation方式
+ *      @Before
+ *     public void init(){
+ *         MockitoAnnotations.initMocks(this);
+ *     }
  *
- * 2.
+ *     @Mock 默认方式
+ *     @Mock(answer = Answers.RETURNS_SMART_NULLS)
+ *  3. rule方式
  */
 package com.zhangll.core.lesson3;
